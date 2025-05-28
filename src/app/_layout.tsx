5fb5d9f6d5e9f4f840c72006f16ba5329e1652f0
edvxndxvi <edvandavi1@hotmail.com>
2025-05-28 13:34:34 -0300

    feat: implementing nativewind and structuring folders

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import '../styles/global.css';
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
